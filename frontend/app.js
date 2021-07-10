44b2476b4f2743d4b95633572519127eed6b96b2
Aadhi Nana <ajibaa@gmail.com>
2021-07-10 13:37:39 +0530

    got web3 working finally. GOD

--- frontend/app.js
+++ frontend/app.js
@@ -1,131 +1,47 @@
-// Check if using modern browsers
-let provider;
-if (window.ethereum) {
-  provider = window.ethereum;
-  try {
-    // Request account access
-    await window.ethereum.request({ method: "eth_requestAccounts" });
-  } catch (error) {
-    // User denied account access...
-    console.error("User denied account access");
+// Import the contract ABI and metadata that is compile
+// Cannot import .json files with import so work around that.
+import contract from "./contract.js";
+
+let web3;
+async function setup() {
+  // Check if using modern browsers
+  let provider;
+  if (window.ethereum) {
+    provider = window.ethereum;
+    try {
+      // Request account access
+      await window.ethereum.request({ method: "eth_requestAccounts" });
+    } catch (error) {
+      // User denied account access...
+      console.error("User denied account access");
+    }
   }
+  // Legacy dapp browsers or older version of metamask...
+  else if (window.web3) {
+    provider = window.web3.currentProvider;
+  }
+  // If no injected web3 instance is detected, fall back to Ganache
+  else {
+    provider = new Web3.providers.HttpProvider("http://localhost:7545");
+  }
+  web3 = new Web3(provider);
+  console.log(web3.version);
+
+  getAccounts();
+  getContract();
 }
-// Legacy dapp browsers or older version of metamask...
-else if (window.web3) {
-  provider = window.web3.currentProvider;
+
+setup();
+
+async function getAccounts() {
+  console.log(await web3.eth.getAccounts());
 }
-// If no injected web3 instance is detected, fall back to Ganache
-else {
-  provider = new Web3.providers.HttpProvider(
-    "http://localhost:7545"
+
+// Contract definition will take ABI and ADDR
+async function getContract() {
+  let instance = new web3.eth.Contract(
+    contract.abi,
+    "0xeD91dae3c4EaF4c43B42c28FB48626EDA05654c5"
   );
+  console.log(instance.methods);
 }
-web3 = new Web3(provider);
-
-let abi = [
-  {
-    "inputs": [
-      {
-        "internalType": "uint256",
-        "name": "_minimum",
-        "type": "uint256"
-      }
-    ],
-    "stateMutability": "nonpayable",
-    "type": "constructor"
-  },
-  {
-    "inputs": [],
-    "name": "minimumContribution",
-    "outputs": [
-      {
-        "internalType": "uint256",
-        "name": "",
-        "type": "uint256"
-      }
-    ],
-    "stateMutability": "view",
-    "type": "function",
-    "constant": true
-  },
-  {
-    "inputs": [],
-    "name": "owner",
-    "outputs": [
-      {
-        "internalType": "address",
-        "name": "",
-        "type": "address"
-      }
-    ],
-    "stateMutability": "view",
-    "type": "function",
-    "constant": true
-  },
-  {
-    "inputs": [],
-    "name": "getTotalAmount",
-    "outputs": [
-      {
-        "internalType": "uint256",
-        "name": "",
-        "type": "uint256"
-      }
-    ],
-    "stateMutability": "view",
-    "type": "function",
-    "constant": true
-  },
-  {
-    "inputs": [],
-    "name": "getAllParticipants",
-    "outputs": [
-      {
-        "internalType": "address[]",
-        "name": "",
-        "type": "address[]"
-      }
-    ],
-    "stateMutability": "view",
-    "type": "function",
-    "constant": true
-  },
-  {
-    "inputs": [],
-    "name": "participate",
-    "outputs": [],
-    "stateMutability": "payable",
-    "type": "function",
-    "payable": true
-  },
-  {
-    "inputs": [],
-    "name": "pickWinner",
-    "outputs": [
-      {
-        "internalType": "address",
-        "name": "",
-        "type": "address"
-      }
-    ],
-    "stateMutability": "view",
-    "type": "function",
-    "constant": true
-  },
-  {
-    "inputs": [
-      {
-        "internalType": "address payable",
-        "name": "_to",
-        "type": "address"
-      }
-    ],
-    "name": "sendMoney",
-    "outputs": [],
-    "stateMutability": "nonpayable",
-    "type": "function"
-  }
-];
-
-console.log(web3.version)
-let contract =  await web3.eth.contract(abi, "0x7a2787e93c07d78bf093E0Bcc8D69B7FB8335335")
